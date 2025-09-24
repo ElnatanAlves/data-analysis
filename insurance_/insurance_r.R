cb968073c8b70d3300f63029da6f0670a2d20a44
Elnatan Alves <156375539+ElnatanAlves@users.noreply.github.com>
2025-09-23 23:58:00 -0300

    Create insurance_r.R

---
+++ insurance_/insurance_r.R
@@ -0,0 +1,10 @@
+df = read.csv('insurance.csv')
+
+#Exibição de dados
+head(df) # Primeiras Linhas
+tail(df) # Ultimas Linhas
+
+#Resumo estatisco de todas as colunas do dataset
+summary(df)
+
+hist(df$age)
